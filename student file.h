49b23baf6d5a8f1c02c73431556d54c6ea7f0999
fernandopdc30 <91210708+fernandopdc30@users.noreply.github.com>
2023-11-24 21:03:23 -0500

    Update student file.h

--- student file.h
+++ student file.h
@@ -1,66 +1,112 @@
-import java.util.*;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-public class ReconstruirCarreteras {
+#define MAX_CARRETERAS 50
+#define MAX_ELEMENTO_CARRETERA 50
 
-    public static String reconstruye(String[] carreteras) {
-        Map<String, Integer> costoTotal = new HashMap<>();
-        Map<String, String> padre = new HashMap<>();
+typedef struct {
+    char id[MAX_ELEMENTO_CARRETERA];
+    char ciudad1[MAX_ELEMENTO_CARRETERA];
+    char ciudad2[MAX_ELEMENTO_CARRETERA];
+    int costo;
+} Carretera;
 
-        for (String carretera : carreteras) {
-            String[] partes = carretera.split(" ");
-            String id = partes[0];
-            String ciudad1 = partes[1];
-            String ciudad2 = partes[2];
+typedef struct {
+    char padre[MAX_ELEMENTO_CARRETERA];
+    int costoTotal;
+} Ciudad;
 
-            if (!costoTotal.containsKey(ciudad1)) {
-                costoTotal.put(ciudad1, 0);
-                padre.put(ciudad1, ciudad1);
-            }
-            if (!costoTotal.containsKey(ciudad2)) {
-                costoTotal.put(ciudad2, 0);
-                padre.put(ciudad2, ciudad2);
-            }
+void inicializarCiudades(Ciudad ciudades[], int numCiudades) {
+    for (int i = 0; i < numCiudades; i++) {
+        strcpy(ciudades[i].padre, "");
+        ciudades[i].costoTotal = 0;
+    }
+}
 
-            int costo = partes.length == 4 ? Integer.parseInt(partes[3]) : 0;
-            costoTotal.put(ciudad1, costoTotal.get(ciudad1) + costo);
-            costoTotal.put(ciudad2, costoTotal.get(ciudad2) + costo);
+int encontrarPadre(Ciudad ciudades[], int ciudad) {
+    if (strcmp(ciudades[ciudad].padre, "") == 0) {
+        return ciudad;
+    } else {
+        return encontrarPadre(ciudades, atoi(ciudades[ciudad].padre));
+    }
+}
 
-            String padreCiudad1 = encontrarPadre(padre, ciudad1);
-            String padreCiudad2 = encontrarPadre(padre, ciudad2);
+void unirCiudades(Ciudad ciudades[], int ciudad1, int ciudad2) {
+    int padreCiudad1 = encontrarPadre(ciudades, ciudad1);
+    int padreCiudad2 = encontrarPadre(ciudades, ciudad2);
 
-            if (!padreCiudad1.equals(padreCiudad2)) {
-                padre.put(padreCiudad1, padreCiudad2);
-            }
-        }
+    if (padreCiudad1 != padreCiudad2) {
+        sprintf(ciudades[padreCiudad1].padre, "%d", padreCiudad2);
+    }
+}
 
-        Set<String> componentesConexas = new HashSet<>();
-        for (String ciudad : costoTotal.keySet()) {
-            componentesConexas.add(encontrarPadre(padre, ciudad));
-        }
+int compararCarreteras(const void *a, const void *b) {
+    return strcmp(((Carretera *)a)->id, ((Carretera *)b)->id);
+}
 
-        if (componentesConexas.size() > 1) {
-            return "IMPOSIBLE";
-        }
+char* reconstruye(char* carreteras[], int numCarreteras) {
+    Carretera arregloCarreteras[MAX_CARRETERAS];
+    Ciudad ciudades[MAX_CARRETERAS];
 
-        String resultado = "";
-        PriorityQueue<String> opciones = new PriorityQueue<>();
-        for (String id : costoTotal.keySet()) {
-            if (costoTotal.get(id) > 0) {
-                opciones.add(id);
-            }
-        }
+    for (int i = 0; i < MAX_CARRETERAS; i++) {
+        strcpy(arregloCarreteras[i].id, "");
+        strcpy(arregloCarreteras[i].ciudad1, "");
+        strcpy(arregloCarreteras[i].ciudad2, "");
+        arregloCarreteras[i].costo = 0;
 
-        while (!opciones.isEmpty()) {
-            resultado += opciones.poll() + " ";
-        }
-
-        return resultado.trim();
+        strcpy(ciudades[i].padre, "");
+        ciudades[i].costoTotal = 0;
     }
 
-    private static String encontrarPadre(Map<String, String> padre, String ciudad) {
-        if (!padre.get(ciudad).equals(ciudad)) {
-            padre.put(ciudad, encontrarPadre(padre, padre.get(ciudad)));
+    for (int i = 0; i < numCarreteras; i++) {
+        sscanf(carreteras[i], "%s %s %s %d", arregloCarreteras[i].id, arregloCarreteras[i].ciudad1,
+               arregloCarreteras[i].ciudad2, &arregloCarreteras[i].costo);
+
+        if (arregloCarreteras[i].costo == 0) {
+            ciudades[atoi(arregloCarreteras[i].ciudad1)].costoTotal += 1;
+            ciudades[atoi(arregloCarreteras[i].ciudad2)].costoTotal += 1;
         }
-        return padre.get(ciudad);
     }
+
+    qsort(arregloCarreteras, numCarreteras, sizeof(Carretera), compararCarreteras);
+
+    inicializarCiudades(ciudades, MAX_CARRETERAS);
+
+    for (int i = 0; i < numCarreteras; i++) {
+        if (arregloCarreteras[i].costo > 0) {
+            int ciudad1 = atoi(arregloCarreteras[i].ciudad1);
+            int ciudad2 = atoi(arregloCarreteras[i].ciudad2);
+
+            unirCiudades(ciudades, ciudad1, ciudad2);
+        }
+    }
+
+    int numComponentesConexas = 0;
+    int componenteConexa = -1;
+
+    for (int i = 0; i < MAX_CARRETERAS; i++) {
+        if (ciudades[i].costoTotal > 0) {
+            if (strcmp(ciudades[i].padre, "") == 0) {
+                numComponentesConexas++;
+                componenteConexa = i;
+            }
+        }
+    }
+
+    if (numComponentesConexas > 1) {
+        return "IMPOSIBLE";
+    }
+
+    char* resultado = (char*)malloc(MAX_CARRETERAS * MAX_ELEMENTO_CARRETERA);
+    strcpy(resultado, "");
+
+    for (int i = 0; i < numCarreteras; i++) {
+        if (arregloCarreteras[i].costo > 0) {
+            strcat(resultado, arregloCarreteras[i].id);
+            strcat(resultado, " ");
+        }
+    }
+
+    return resultado;
 }
