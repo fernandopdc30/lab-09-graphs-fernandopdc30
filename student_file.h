28b50c12ccc5d0629c9b14103d7dad209f1fe769
fernandopdc30 <91210708+fernandopdc30@users.noreply.github.com>
2023-11-25 18:13:38 -0500

    Update student_file.h

--- student_file.h
+++ student_file.h
@@ -10,118 +10,112 @@ using namespace std;
 
 class Union {
 private:
-  map<string, string> padre;
-  map<string, int> rango;
+    map<string, string> padre;
+    map<string, int> rango;
 
 public:
-  void hacerConjunto(const string& elemento) {
-    padre[elemento] = elemento;
-    rango[elemento] = 0;
-  }
-
-  string encontrar(const string& elemento) {
-    if (padre[elemento] != elemento)
-      padre[elemento] = encontrar(padre[elemento]);
-    return padre[elemento];
-  }
-
-  bool unirConjuntos(const string& a, const string& b) {
-    string raizA = encontrar(a);
-    string raizB = encontrar(b);
-
-    if (raizA == raizB) {
-      return false;
+    void hacerConjunto(const string& elemento) {
+        padre[elemento] = elemento;
+        rango[elemento] = 0;
     }
 
-    if (rango[raizA] < rango[raizB]) {
-      padre[raizA] = raizB;
-    } else if (rango[raizA] > rango[raizB]) {
-      padre[raizB] = raizA;
-    } else {
-      padre[raizB] = raizA;
-      rango[raizA]++;
+    string encontrar(const string& elemento) {
+        if (padre[elemento] != elemento)
+            padre[elemento] = encontrar(padre[elemento]);
+        return padre[elemento];
     }
 
-    return true;
-  }
+    bool unirConjuntos(const string& a, const string& b) {
+        string raizA = encontrar(a);
+        string raizB = encontrar(b);
+
+        if (raizA == raizB) {
+            return false;
+        }
+
+        if (rango[raizA] < rango[raizB]) {
+            padre[raizA] = raizB;
+        } else if (rango[raizA] > rango[raizB]) {
+            padre[raizB] = raizA;
+        } else {
+            padre[raizB] = raizA;
+            rango[raizA]++;
+        }
+
+        return true;
+    }
 };
 
 class Carretera {
 public:
-  string id;
-  string ciudad1;
-  string ciudad2;
-  int costo;
+    string id;
+    string ciudad1;
+    string ciudad2;
+    int costo;
 
-  Carretera(string i, string c1, string c2, int co = 0) : id(i), ciudad1(c1), ciudad2(c2), costo(co) {}
+    Carretera(string i, string c1, string c2, int co = 0) : id(i), ciudad1(c1), ciudad2(c2), costo(co) {}
 };
 
-// Inicializa conjuntos para cada ciudad.
 void inicializarConjuntos(Union& unionCiudad, set<string>& ciudades, const vector<string>& listaCarreteras) {
-  for (const auto& carreteraStr : listaCarreteras) {
-    stringstream ss(carreteraStr);
-    string id, ciudad1, ciudad2;
-    ss >> id >> ciudad1 >> ciudad2;
-    unionCiudad.hacerConjunto(ciudad1);
-      unionCiudad.hacerConjunto(ciudad2);
-    ciudades.insert(ciudad1);
-    ciudades.insert(ciudad2);
-  }
+    for (const auto& carreteraStr : listaCarreteras) {
+        stringstream ss(carreteraStr);
+        string id, ciudad1, ciudad2;
+        ss >> id >> ciudad1 >> ciudad2;
+        unionCiudad.hacerConjunto(ciudad1);
+        unionCiudad.hacerConjunto(ciudad2);
+        ciudades.insert(ciudad1);
+        ciudades.insert(ciudad2);
+    }
 }
 
-// Verifica que todas las ciudades estén conectadas.
 bool todasCiudadesConectadas(Union& unionCiudad, const set<string>& ciudades) {
-  string raiz = unionCiudad.encontrar(*ciudades.begin());
-  for (const auto& ciudad : ciudades) {
-    if (unionCiudad.encontrar(ciudad) != raiz) {
-      return false;
+    string raiz = unionCiudad.encontrar(*ciudades.begin());
+    for (const auto& ciudad : ciudades) {
+        if (unionCiudad.encontrar(ciudad) != raiz) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
-// Reconstruye y devuelve la lista de carreteras seleccionadas.
 string reconstruye(vector<string> listaCarreteras) {
-  vector<Carretera> carreterasDeterioradas;
-  Union unionCiudad;
-  set<string> ciudades;
-  set<string> carreterasSeleccionadas;
+    vector<Carretera> carreterasDeterioradas;
+    Union unionCiudad;
+    set<string> ciudades;
+    set<string> carreterasSeleccionadas;
 
-  inicializarConjuntos(unionCiudad, ciudades, listaCarreteras);
+    inicializarConjuntos(unionCiudad, ciudades, listaCarreteras);
 
-  for (const auto& carreteraStr : listaCarreteras) {
-    stringstream ss(carreteraStr);
-    string id, ciudad1, ciudad2;
-    int costo = 0;
-    ss >> id >> ciudad1 >> ciudad2;
-    if (!(ss >> costo)) {
-        unionCiudad.unirConjuntos(ciudad1, ciudad2);
+    for (const auto& carreteraStr : listaCarreteras) {
+        stringstream ss(carreteraStr);
+        string id, ciudad1, ciudad2;
+        int costo = 0;
+        ss >> id >> ciudad1 >> ciudad2;
+        if (!(ss >> costo)) {
+            unionCiudad.unirConjuntos(ciudad1, ciudad2);
+        } else {
+            carreterasDeterioradas.emplace_back(id, ciudad1, ciudad2, costo);
+        }
+    }
+
+    sort(carreterasDeterioradas.begin(), carreterasDeterioradas.end(), [](const Carretera& a, const Carretera& b) {
+        return (a.costo != b.costo) ? a.costo < b.costo : a.id < b.id;
+    });
+
+    for (const auto& carretera : carreterasDeterioradas) {
+        if (unionCiudad.encontrar(carretera.ciudad1) != unionCiudad.encontrar(carretera.ciudad2)) {
+            unionCiudad.unirConjuntos(carretera.ciudad1, carretera.ciudad2);
+            carreterasSeleccionadas.insert(carretera.id);
+        }
+    }
+
+    if (todasCiudadesConectadas(unionCiudad, ciudades)) {
+        string resultado;
+        for (const auto& id : carreterasSeleccionadas) {
+            resultado += id + " ";
+        }
+        return resultado.empty() ? "" : resultado.substr(0, resultado.length() - 1);
     } else {
-      carreterasDeterioradas.emplace_back(id, ciudad1, ciudad2, costo);
+        return "IMPOSIBLE";
     }
-  }
-
-  sort(carreterasDeterioradas.begin(), carreterasDeterioradas.end(), [](const Carretera& a, const Carretera& b) {
-    if (a.costo != b.costo) {
-      return a.costo < b.costo;
-    }
-    return a.id < b.id;
-  });
-
-  for (const auto& carretera : carreterasDeterioradas) {
-    if (unionCiudad.encontrar(carretera.ciudad1) != unionCiudad.encontrar(carretera.ciudad2)) {
-          unionCiudad.unirConjuntos(carretera.ciudad1, carretera.ciudad2);
-      carreterasSeleccionadas.insert(carretera.id);
-    }
-  }
-
-  if (todasCiudadesConectadas(unionCiudad, ciudades)) {
-    string resultado;
-    for (const auto& id : carreterasSeleccionadas) {
-      resultado += id + " ";
-    }
-    return resultado.empty() ? "" : resultado.substr(0, resultado.length() - 1);
-  } else {
-    return "IMPOSIBLE";
-  }
 }
